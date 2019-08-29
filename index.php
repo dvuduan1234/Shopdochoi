c2a43743a9a2af2dc3d2c9bfd5e0e702d4f6985d
dvuduan1234 <53039161+dvuduan1234@users.noreply.github.com>
2019-08-29 13:09:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TOCOTOCO SHOP</title>
+        <title>Shopdochoi</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <style>
